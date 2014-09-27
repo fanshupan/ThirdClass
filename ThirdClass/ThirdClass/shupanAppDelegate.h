45cbe1482a77483e944387c60aa16be5cdd52c96
fanshupan <user1@Wus-MacBook-Pro.local>
2014-09-27 12:48:11 +0800

    sdf
    sdf

---
+++ ThirdClass/ThirdClass/shupanAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  shupanAppDelegate.h
+//  ThirdClass
+//
+//  Created by user1 on 14-9-27.
+//  Copyright (c) 2014年 ThirdClass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface shupanAppDelegate : UIResponder <UIApplicationDelegate>
+@property (nonatomic,strong) UIWindow *window;
+@end
---
+++ ThirdClass/ThirdClass/shupanAppDelegate.m
@@ -0,0 +1,82 @@
+//
+//  shupanAppDelegate.m
+//  ThirdClass
+//
+//  Created by user1 on 14-9-27.
+//  Copyright (c) 2014年 ThirdClass. All rights reserved.
+//
+
+#import "shupanAppDelegate.h"
+
+@implementation shupanAppDelegate
+
+
+
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    NSLog(@"will termin");
+    
+    
+    
+    
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    NSLog(@"did finish laching");
+    self.window = [[UIWindow alloc] initWithFrame:CGRectMake(0, 100, 300, 300)];
+    [self.window makeKeyAndVisible];
+    self.window.backgroundColor = [UIColor whiteColor];
+    UIView *myview = [[UIView alloc] initWithFrame:CGRectMake(10, 50, 100, 100)];
+    myview.backgroundColor = [UIColor greenColor];
+    UIView *myview2 = [[UIView alloc] initWithFrame:CGRectMake(10, 10, 50, 50)];
+    [myview2 setBackgroundColor:[UIColor redColor]];
+    
+    [self.window addSubview:myview2];
+   // myview.bounds
+    
+    NSLog(@"myview frmae x %f",myview.frame.origin.x);
+    NSLog(@"myview bounds x %f",myview.center.x);
+    
+    
+   myview.center = CGPointMake(200, 100);
+    
+    
+    [self.window addSubview:myview];
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    NSLog(@"resign active");
+    
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    NSLog(@"did enter");
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    NSLog(@"enter foreground");
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    NSLog(@"appli did becom");
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+
+
+@end
