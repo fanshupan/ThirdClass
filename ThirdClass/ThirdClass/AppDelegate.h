36d387359b9667bb7614f837143092af4fd9e1dd
fanshupan <user1@Wus-MacBook-Pro.local>
2014-09-27 10:16:11 +0800

    ds
    fvdv

---
+++ ThirdClass/ThirdClass/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ThirdClass
+//
+//  Created by user1 on 14-9-27.
+//  Copyright (c) 2014年 ThirdClass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ThirdClass/ThirdClassTests/ThirdClassTests.m
@@ -0,0 +1,34 @@
+//
+//  ThirdClassTests.m
+//  ThirdClassTests
+//
+//  Created by user1 on 14-9-27.
+//  Copyright (c) 2014年 ThirdClass. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ThirdClassTests : XCTestCase
+
+@end
+
+@implementation ThirdClassTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
